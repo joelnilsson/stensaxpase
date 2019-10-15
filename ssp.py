f57f9374848efd0bf2b3fedbf2fbc814f740a6f3
Joel Nilsson <joel.nilsson9@learnet.se>
2019-10-15 10:20:26 +0200

    sekvens 3 färdig

--- ssp.py
+++ ssp.py
@@ -1,25 +1,36 @@
 import random
+import getpass
 loop = False
 
 
 print("Välkommen till sten, sax, påse!")
 
-while loop = False:
+while loop == False:
     print('''
-    1. Starta spel
-    2. Avsluta''')
-    avsluta = input("Välj alternativ")
+    1. Spela ensam
+    2. Spela med någon
+    3. Avsluta''')
+    avsluta = int(input("Välj alternativ: "))
     if avsluta == 1:
         print("Du spelar mot en dator")
-        datorVal = random()
+        datorVal = random.random(1,3)
         print('''
         1. Sten
         2. Sax
         3. Påse''')
         Val = input("Välj ett alternativ")
-
-
     elif avsluta == 2:
+        print("Två spelare")
+        playerOneName = input("Ange namn för spelare 1")
+        playerTwoName = input("Ange namn för spelare 2")
+        print('''
+        1. Sten
+        2. Sax
+        3. Påse''')
+        playerOneC = getpass.getpass(playerOneName, " väljer: ")
+        playerTwoC = getpass.getpass(playerTwoName, " väljer: ")
+
+    elif avsluta == 3:
         exit("Tack för att du spelade") 
     else:
         print("Var vänlig välj ett giltigt alternativ")
