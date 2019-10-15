76b8e131dd1552fe127d2348aeba1f5ce3d94872
Joel Nilsson <joel.nilsson9@learnet.se>
2019-10-15 11:08:22 +0200

    sekvens 4.5

--- ssp.py
+++ ssp.py
@@ -1,34 +1,45 @@
+#importerar random och getpass samt gör variabeln loop till false
 import random
 import getpass
 loop = False
 
 
 print("Välkommen till sten, sax, påse!")
-
+#game loop
 while loop == False:
     print('''
     1. Spela ensam
     2. Spela med någon
     3. Avsluta''')
     try:
-         avsluta = int(input("Välj alternativ: "))
+        avsluta = int(input("Välj alternativ: ")) #byt namn på variabeln
          
     except (ValueError):
         avsluta = 4
     if avsluta == 1:
         print("Du spelar mot en dator")
+        rundor = 0
+        win = False
         while win == False:
-            
+            if rundor > 3:
+                win = True
             datorVal = random.randint(1,3)
             print('''
             1. Sten
             2. Sax
             3. Påse''')
-            Val = input("Välj ett alternativ")
+            val = int(input("Välj ett alternativ"))
             if val == datorVal:
                 print("Ni valde samma alternativ, spela igen")
-            elif val == 1 and 
-        
+            elif val == 1 and datorVal == 2:
+                print('''Spelare valde sten, dator valde sax
+                spelare vinner''')
+                rundor = rundor + 1
+            elif val == 1 and datorVal == 3:
+                print('''Spelare valde sten, dator valde påse
+                dator vinner''')
+#Fortsätt ange Elifs om logiken
+
     elif avsluta == 2:
         print("Två spelare")
         playerOneName = input("Ange namn för spelare 1")
@@ -39,6 +50,8 @@ while loop == False:
         3. Påse''')
         playerOneC = getpass.getpass("Spelare 1 väljer: ")
         playerTwoC = getpass.getpass("Spelare 2 väljer: ")
+
+#gör klart spelet mot datorn innan vi fortsätter göra logiken mot en annan spelare
 
     elif avsluta == 3:
         exit("Tack för att du spelade") 
