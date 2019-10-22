c05dad0293102e2c402d35be490b6551dba9d36b
Joel Nilsson <joel.nilsson9@learnet.se>
2019-10-22 10:49:34 +0200

    antal rundor

--- ssp.py
+++ ssp.py
@@ -1,6 +1,7 @@
 #importerar random och getpass samt gör variabeln loop till false
 import random
 import getpass
+import time
 loop = False
 
 
@@ -22,9 +23,17 @@ while loop == False:
         win = False
         playerScore = 0
         compScore = 0
+        antalRundor = int(input("Hur många rundor vill du spela?: "))
+        antalRundor = antalRundor - 1
         while win == False:
-            if rundor > 2:
-                if playerScore > compScore:
+            if rundor > antalRundor:
+                if playerScore == compScore:
+                    print('''
+                    
+                    Det blev oavgjort
+                    ''', playerScore, "-", compScore, '''
+                    Tack för att du spelade''')
+                else:
                     print('''
                     
                     
@@ -32,56 +41,122 @@ while loop == False:
                     ''', playerScore, "-", compScore, '''
                     Tack för att du spelade''')
                 win = True
-            datorVal = random.randint(1,3)
-            print('''
-            1. Sten
-            2. Sax
-            3. Påse''')
-            val = int(input("Välj ett alternativ: "))
-            if val == datorVal:
-                print("Ni valde samma alternativ, spela igen")
-            elif val == 1 and datorVal == 2:
-                print('''Spelare valde sten, dator valde sax
-                spelare vinner''')
-                playerScore = playerScore + 1
-                rundor = rundor + 1
-            elif val == 1 and datorVal == 3:
-                print('''Spelare valde sten, dator valde påse
-                dator vinner''')
-                compScore = compScore + 1
-                rundor = rundor + 1
-            elif val == 2 and datorVal == 1:
-                print('''Spelare valde sax, dator valde sten
-                dator vinner''')
-                compScore = compScore + 1
-                rundor = rundor + 1
-            elif val == 2 and datorVal == 3:
-                print('''Spelare valde sax, dator valde påse
-                spelare vinner''')
-                playerScore = playerScore + 1
-                rundor = rundor + 1
-            elif val == 3 and datorVal == 1:
-                print('''Spelare valde påse, dator valde sten
-                spelare vinner''')
-                playerScore = playerScore + 1
-                rundor = rundor + 1
-            elif val == 3 and datorVal == 2:
-                print('''Spelare valde påse, dator valde sax
-                dator vinner''')
-                compScore = compScore + 1
-                rundor = rundor + 1
+            
+            else:
+                time.sleep(1)
+                datorVal = random.randint(1,3)
+                print('''
+                1. Sten
+                2. Sax
+                3. Påse''')
+                val = int(input("Välj ett alternativ: "))
+                if val == datorVal:
+                    print("Ni valde samma alternativ, spela igen")
+                elif val == 1 and datorVal == 2:
+                    print('''Spelare valde sten, dator valde sax
+                    spelare vinner''')
+                    playerScore = playerScore + 1
+                    rundor = rundor + 1
+                elif val == 1 and datorVal == 3:
+                    print('''Spelare valde sten, dator valde påse
+                    dator vinner''')
+                    compScore = compScore + 1
+                    rundor = rundor + 1
+                elif val == 2 and datorVal == 1:
+                    print('''Spelare valde sax, dator valde sten
+                    dator vinner''')
+                    compScore = compScore + 1
+                    rundor = rundor + 1
+                elif val == 2 and datorVal == 3:
+                    print('''Spelare valde sax, dator valde påse
+                    spelare vinner''')
+                    playerScore = playerScore + 1
+                    rundor = rundor + 1
+                elif val == 3 and datorVal == 1:
+                    print('''Spelare valde påse, dator valde sten
+                    spelare vinner''')
+                    playerScore = playerScore + 1
+                    rundor = rundor + 1
+                elif val == 3 and datorVal == 2:
+                    print('''Spelare valde påse, dator valde sax
+                    dator vinner''')
+                    compScore = compScore + 1
+                    rundor = rundor + 1
 #Fortsätt ange Elifs om logiken
 
     elif avsluta == 2:
         print("Två spelare")
-        playerOneName = input("Ange namn för spelare 1")
-        playerTwoName = input("Ange namn för spelare 2")
-        print('''
-        1. Sten
-        2. Sax
-        3. Påse''')
-        playerOneC = getpass.getpass("Spelare 1 väljer: ")
-        playerTwoC = getpass.getpass("Spelare 2 väljer: ")
+        playerOneName = input("Ange namn för spelare 1: ")
+        playerTwoName = input("Ange namn för spelare 2: ")
+        win = False
+        playerOneScore = 0
+        playerTwoScore = 0
+        rundor = 0
+        antalRundor = int(input("Hur många rundor vill du spela?: "))
+        antalRundor = antalRundor - 1
+        while win == False:
+            if playerOneScore > playerTwoScore:
+                winner = playerOneName
+            else:
+                winner = playerTwoName 
+            if rundor > antalRundor:
+                if playerOneScore == playerTwoScore:
+                    print('''
+                    
+                    Det blev oavgjort
+                    ''', playerOneScore, "-", playerTwoScore, '''
+                    Tack för att du spelade''')
+                else:
+                    print('''
+                    
+                    ''',
+                    winner, '''har vunnit
+                    ''', playerOneScore, "-", playerTwoScore, '''
+                    Tack för att du spelade''')
+                win = True
+            else:
+                time.sleep(1)
+                print('''
+                1. Sten
+                2. Sax
+                3. Påse
+                ''')
+                playerOneC = getpass.getpass("Spelare 1 väljer: ")
+                playerTwoC = getpass.getpass("Spelare 2 väljer: ")
+                playerOneC = int(playerOneC)
+                playerTwoC = int(playerTwoC)
+                if playerOneC == playerTwoC:
+                    print("Ni valde samma alternativ, spela igen")
+                elif playerOneC == 1 and playerTwoC == 2:
+                    print('''Spelare 1 valde sten, spelare 2 valde sax
+                    spelare 1 vinner''')
+                    playerOneScore = playerOneScore + 1
+                    rundor = rundor + 1
+                elif playerOneC == 1 and playerTwoC == 3:
+                    print('''Spelare 1 valde sten, spelare 2 valde påse
+                    spelare 2 vinner''')
+                    playerTwoScore = playerTwoScore + 1
+                    rundor = rundor + 1
+                elif playerOneC == 2 and playerTwoC == 1:
+                    print('''Spelare 1 valde sax, spelare 2 valde sten
+                    spelare 2 vinner''')
+                    playerTwoScore = playerTwoScore + 1
+                    rundor = rundor + 1
+                elif playerOneC == 2 and playerTwoC == 3:
+                    print('''Spelare 1 valde sax, spelare 2 valde påse
+                    spelare 1 vinner''')
+                    playerOneScore = playerOneScore + 1
+                    rundor = rundor + 1
+                elif playerOneC == 3 and playerTwoC == 1:
+                    print('''Spelare 1 valde påse, spelare 2 valde sten
+                    spelare 1 vinner''')
+                    playerOneScore = playerOneScore + 1
+                    rundor = rundor + 1
+                elif playerOneC == 3 and playerTwoC == 2:
+                    print('''Spelare 1 valde påse, spelare 2 valde sax
+                    spelare 2 vinner''')
+                    playerTwoScore = playerTwoScore + 1
+                    rundor = rundor + 1
 
 #gör klart spelet mot datorn innan vi fortsätter göra logiken mot en annan spelare
 
